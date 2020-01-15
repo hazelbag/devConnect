9c800786a71809bddc08251e0a0a925ca3976a9b
hazelbag <jacques.o@outlook.com>
2020-01-15 02:40:59 +0200

    Auth routes and redirects

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "./types";
 import setAuthToken from '../utils/setAuthToken'
 
@@ -44,7 +45,7 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
-    
+
     dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
@@ -85,3 +86,8 @@ export const login = (email, password) => async dispatch => {
     });
   }
 };
+
+// Logout and clear the profile
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT })
+}
