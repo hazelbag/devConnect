92be5605e34bc914bb6fd1906a97b53c95778ce7
hazelbag <jacques.o@outlook.com>
2020-01-16 16:13:11 +0200

    Finalized all componenets for the APP

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,7 +4,9 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from '../actions/types'
 
 const initialState = {
@@ -29,12 +31,12 @@ export default function (state = initialState, action) {
                 post: payload,
                 loading: false
             }
-        case ADD_POST: 
-        return {
-            ...state,
-            posts: [payload, ...state.posts],
-            loading: false
-        };
+        case ADD_POST:
+            return {
+                ...state,
+                posts: [payload, ...state.posts],
+                loading: false
+            };
         case DELETE_POST:
             return {
                 ...state,
@@ -57,7 +59,24 @@ export default function (state = initialState, action) {
                     post
                 ),
                 loading: false
+            };
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: { ...state.post, comments: payload },
+                loading: false
             }
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(
+                        comment => comment._id !== payload
+                    )
+                },
+                loading: false
+            };
         default:
             return state;
     }
